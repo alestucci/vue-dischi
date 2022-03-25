6b16331b6e6bc0d96ced851dfcadb06524a5fc6d
alestucci <alessandrotucci@gmail.com>
2022-03-25 15:01:25 +0100

    Axion implemented

--- src/components/AlbumCard.vue
+++ src/components/AlbumCard.vue
@@ -1,13 +1,10 @@
 <template>
   <div>
     <div class="card text-center">
-      <img
-        src="https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg"
-        alt="Album"
-      />
-      <h2 class="py-3 fs-5">New Jersey</h2>
-      <h3 class="fs-5 fw-light">Bon Jovi</h3>
-      <h4 class="fs-5 fw-light pb-4">1988</h4>
+      <img :src="albumData.poster" :alt="albumData.title" />
+      <h2 class="py-3 fs-5">{{ albumData.title }}</h2>
+      <h3 class="fs-5 fw-light">{{ albumData.author }}</h3>
+      <h4 class="fs-5 fw-light pb-4">{{ albumData.year }}</h4>
     </div>
   </div>
 </template>
@@ -15,11 +12,15 @@
 <script>
 export default {
   name: "AlbumCard",
+  props: {
+    albumData: Object,
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .card {
+  min-height: 420px;
   background-color: #2e3a46;
   padding: 1rem;
 }
--- src/components/SpotifyMain.vue
+++ src/components/SpotifyMain.vue
@@ -1,19 +1,18 @@
 <template>
   <main>
     <div class="container">
+      <h1 v-if="albumsArray === null" class="text-center">
+        Dati in caricamento
+      </h1>
       <div
+        v-else
         class="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 gy-3 gx-5"
       >
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
-        <album-card />
+        <album-card
+          v-for="album in albumsArray"
+          :key="album.title"
+          :album-data="album"
+        />
       </div>
     </div>
   </main>
@@ -21,19 +20,37 @@
 
 <script>
 import AlbumCard from "./AlbumCard.vue";
+import axios from "axios";
 
 export default {
   name: "SpotifyMain",
+  data() {
+    return {
+      albumsArray: null,
+    };
+  },
   components: {
     AlbumCard,
+  },
+  created() {
+    setTimeout(() => {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/array/music")
+        .then((response) => {
+          this.albumsArray = response.data.response;
+        });
+    }, 2000);
   },
 };
 </script>
 
 <style lang="scss" scoped>
 main {
-  height: calc(100vw - 70px);
+  min-height: calc(100vh - 70px);
   padding: 5rem;
   background-color: #1e2d3b;
+}
+h1 {
+  color: white;
 }
 </style>
